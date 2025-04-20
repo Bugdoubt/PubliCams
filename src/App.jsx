2773fcfbc491ec3965581137e2f4a97179ab4fed
Bugdoubt <kieronmillar@hotmail.com>
2025-04-20 17:25:31 +0100

    Initial clean project commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import WebcamMap from './components/WebcamMap';
+
+function App() {
+  return (
+    <main className="min-h-screen bg-gray-900 text-white p-4">
+      <h1 className="text-2xl font-bold mb-4">Aberdeen Webcam Demo</h1>
+      <WebcamMap />
+    </main>
+  );
+}
+
+export default App;
